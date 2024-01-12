e50c4ef88a39a57b4f99e04bf8b108bd4f55cf3f
Emily <emily@bunni.me>
2024-01-11 23:20:09 -0500

    Update footer links and copyright message

--- src/app/(public)/(layout-components)/Footer.tsx
+++ src/app/(public)/(layout-components)/Footer.tsx
@@ -10,30 +10,30 @@ const Footer = () => (
       <nav className="flex flex-row flex-wrap justify-between gap-12 md:gap-16 lg:gap-28 xl:gap-40 2xl:gap-52">
         <div className="links flex flex-col">
           <h6><b>Company</b></h6>
-          <HashLink href="">About us</HashLink>
-          <HashLink href="">Contact</HashLink>
-          <HashLink href="">Careers</HashLink>
-          <HashLink href="">Press Kit</HashLink>
+          <HashLink href="/about">About us</HashLink>
+          <HashLink href="/contact">Contact</HashLink>
+          <HashLink href="/talent">Careers</HashLink>
+          <HashLink href="/presskit">Press Kit</HashLink>
         </div>
 
         <div className="links flex flex-col">
           <h6><b>Legal</b></h6>
           <HashLink href="/terms">Terms of Service</HashLink>
-          <HashLink href="">Privacy Policy</HashLink>
-          <HashLink href="">Cookie Policy</HashLink>
-          <HashLink href="">Tax Policy</HashLink>
+          <HashLink href="/privacy">Privacy Policy</HashLink>
+          <HashLink href="/cookie">Cookie Policy</HashLink>
+          <HashLink href="/tax">Tax Policy</HashLink>
         </div>
 
         <div className="links flex flex-col">
           <h6><b>Services</b></h6>
-          <HashLink href="">Service Status</HashLink>
-          <HashLink href="">Billing</HashLink>
+          <HashLink href="https://uptime.pinkcloud.studios">Service Status</HashLink>
+          <HashLink href="https://billing.pinkcloud.studios">Billing</HashLink>
         </div>
       </nav>
     </div>
 
     <div className="flex flex-col w-full px-4 py-3 items-start bg-base-300 gap-2">
-      <small className="flex flex-row flex-wrap items-center">Made with<FaHeart className="text-neutral mx-1.5 min-h-3.5 min-w-3.5"/>by Elaina. &copy; Copyright {`${new Date().getFullYear()}`}, PinkCloud Studios LLC.</small>
+      <small className="flex flex-row flex-wrap items-center">Made with<FaHeart className="text-neutral mx-1.5 min-h-3.5 min-w-3.5"/>from NA & SA. &copy; Copyright {`${new Date().getFullYear()}`}, PinkCloud Studios LLC.</small>
     </div>
   </footer>
 );
