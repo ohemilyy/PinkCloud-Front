e09972df70334b379877badb53c0a5c6f6d5557e
Elynn 3 <elynn@meowers.dev>
2024-01-12 01:38:22 -0300

    Fixing Company naming

--- src/app/(public)/(home)/Services.tsx
+++ src/app/(public)/(home)/Services.tsx
@@ -23,20 +23,20 @@ const Services = () => (
     
     <div className="grid auto-rows-fr gap-1 cards w-full max-w-screen-lg mx-auto">
       <Card icon={<FaInfinity />} title="Freelances" href={""}>
-      PinkCloud Studio offers a diverse range of freelance services, specializing in web development, software application development, and various other types of development. Our team is dedicated to providing innovative solutions tailored to meet the unique needs of each client.
+      PinkCloud Studios offers a diverse range of freelance services, specializing in web development, software application development, and various other types of development. Our team is dedicated to providing innovative solutions tailored to meet the unique needs of each client.
       </Card>
       <Card icon={<DiDocker />} title="Application Development" href={""}>
-      PinkCloud Studio specializes in crafting cutting-edge applications tailored to meet your specific needs. Whether you're looking for mobile apps, desktop applications, or custom software solutions, our team is committed to delivering excellence.
+      PinkCloud Studios specializes in crafting cutting-edge applications tailored to meet your specific needs. Whether you're looking for mobile apps, desktop applications, or custom software solutions, our team is committed to delivering excellence.
       </Card>
       <Card icon={<SiNextdotjs />} title="Full-Stack Web Development" href={""}>
-      PinkCloud Studio excels in both frontend and backend web development, ensuring a seamless and powerful online presence for your projects. Our team leverages the latest technologies, including Next.js, to deliver modern and efficient web solutions.      </Card>
+      PinkCloud Studios excels in both frontend and backend web development, ensuring a seamless and powerful online presence for your projects. Our team leverages the latest technologies, including Next.js, to deliver modern and efficient web solutions.      </Card>
       <Card icon={<FaDiscord />} title="Discord Development" href={""}>
-      PinkCloud Studio specializes in Discord development, creating customized solutions to enhance your server's functionality and user experience. Whether it's bots, integrations, or other Discord-related projects, our team is dedicated to bringing your ideas to life.      </Card>
+      PinkCloud Studios specializes in Discord development, creating customized solutions to enhance your server's functionality and user experience. Whether it's bots, integrations, or other Discord-related projects, our team is dedicated to bringing your ideas to life.      </Card>
       <Card icon={<FaTerminal />} title="Infrastructure Management" href={""}>
-      PinkCloud Studio excels in comprehensive infrastructure management, including expert system administration services. We specialize in optimizing the performance, security, and scalability of your systems.
+      PinkCloud Studios excels in comprehensive infrastructure management, including expert system administration services. We specialize in optimizing the performance, security, and scalability of your systems.
       </Card>
       <Card icon={<LuFileTerminal />} title="Infrastructure Development & Consultancy" href={""}>
-      PinkCloud Studio excels in infrastructure development and consultancy, providing robust solutions to meet your technical needs. From setting up scalable server architectures to offering expert advice, we ensure a solid foundation for your projects.
+      PinkCloud Studios excels in infrastructure development and consultancy, providing robust solutions to meet your technical needs. From setting up scalable server architectures to offering expert advice, we ensure a solid foundation for your projects.
       </Card>
     </div>
   </section>
