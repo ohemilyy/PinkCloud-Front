50a33eade8f5e38a3e669e565037f9f17f1aceca
Elynn 3 <elynn@meowers.dev>
2024-02-01 20:36:25 -0300

    Removing Supabase installation

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,16 +1,9 @@
 'use client'
 import './styles.css'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { Database } from '@/libs/supabase/database.types'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
-import { Auth } from '@supabase/auth-ui-react'
 import Logo from '@/components/Logo'
 import Console from './Console';
 
 export default function SignIn() {
-  const supabase = createClientComponentClient<Database>()
-  const callbackRoute = process.env.NEXT_PUBLIC_CURR_DOMAIN + "/api/auth/callback";
-
   return <>
     <div className="flex w-full min-h-screen items-center justify-center bg-full bg-img">
       <Console className="pb-10 pt-4 px-6 sm:px-16">
@@ -18,16 +11,7 @@ export default function SignIn() {
         <h4 className="mb-3">
           Log into our dashboard below.
         </h4>
-        <Auth
-          supabaseClient={supabase}
-          view="magic_link"
-          theme={'dark'}
-          dark={true}
-          appearance={{ theme: ThemeSupa }}
-          showLinks={false}
-          providers={['github', 'discord', 'google']}
-          redirectTo={callbackRoute}
-        />
+    
       </Console>
     </div>
   </>;
