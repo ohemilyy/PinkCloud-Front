5765bae8e729d98bdd15b808ead54653cb79a9bf
Elaina <17bestradiol@proton.me>
2024-02-10 17:00:14 -0300

    Adding Console

--- src/app/(public)/(layout-components)/NavLinks.tsx
+++ src/app/(public)/(layout-components)/NavLinks.tsx
@@ -3,8 +3,7 @@ import useHash from "@/hooks/useHash";
 import HashLink from "@/components/HashLink";
 import { usePathname } from "next/navigation";
 import { FaBlog, FaSatellite, FaSatelliteDish, FaUserAstronaut } from "react-icons/fa6";
-import { FaGlobeAmericas } from "react-icons/fa";
-import { LuShieldAlert } from "react-icons/lu";
+import { HiMiniShieldExclamation } from "react-icons/hi2";
 
 const NavLinks = () => {
   const currRoute = usePathname() + useHash();
@@ -12,7 +11,7 @@ const NavLinks = () => {
     { href: "/dash", text: "Dashboard", icon: <FaSatellite className="w-4 h-4" /> },
     { href: "/blog", text: "Blog", icon: <FaBlog className="w-5 h-5" /> },
     { href: "https://uptime.pinkcloud.studio", text: "Uptime", icon: <FaSatelliteDish className="w-5 h-5" /> },
-    { href: "/hydro", text: "AntiCheat Services", icon: <LuShieldAlert className="w-5 h-5" /> },
+    { href: "/hydro", text: "Anti-Cheat", icon: <HiMiniShieldExclamation className="w-5 h-5 scale-125" /> },
     { href: "/crew", text: "Crew", icon: <FaUserAstronaut className="w-5 h-5" /> },
   ];
 
--- src/app/(public)/hydro/page.tsx
+++ src/app/(public)/hydro/page.tsx
@@ -1,31 +1,37 @@
 'use client';
 import HashLink from '@/components/HashLink';
 import './styles.css';
-import {FaShoppingCart, FaArrowRight} from "react-icons/fa";
+import {FaShoppingCart} from "react-icons/fa";
+import Console from '@/components/Console/component';
+
 export default function Hydro() {
   return (
-    <div>
-      <h1 className="pt-24 text-rainbow text-4xl md:text-5xl lg:text-6xl font-medium font-header lg:max-w-[72rem] mx-auto text-center mb-4">
+    <div className="flex flex-col p-6 pt-24">
+      <h1 className="text-rainbow text-4xl md:text-5xl lg:text-6xl font-medium font-header lg:max-w-[72rem] mx-auto text-center mb-4">
         Hydro Anticheat
       </h1>
       <p className="hero-p">
         The most reliable and trustworthy anti-cheat solution for safeguarding your server's integrity.
       </p>
-      <span className="hero-p">
-          <strong>Try us now</strong>
-        <i className="fas fa-long-arrow-alt-right idk"></i>
-        <HashLink href={process.env.NEXT_PUBLIC_DISCORD_INVITE ?? '/'} className="btn btn-outline btn-secondary min-h-fit h-fit px-5 py-2.5 mt-7">
-          <div className="flex flex-row justify-center items-center pb-0.5 gap-1.5">
-            <FaShoppingCart className="mt-0.5 h-5 w-5"/>
-            <span>Purchase</span>
-          </div>
-        </HashLink>
+
+      <span className="hero-p inline-flex justify-between mt-4">
+        <span className="inline-flex items-start h-fit items-center">
+          <strong className="">Try us now</strong>
+          <i className="fas fa-long-arrow-alt-right idk"></i>
+          <HashLink href={process.env.NEXT_PUBLIC_DISCORD_INVITE ?? '/'} className="btn btn-outline btn-secondary min-h-fit h-fit px-5 py-2.5">
+            <div className="flex flex-row justify-center items-center pb-0.5 gap-1.5">
+              <FaShoppingCart className="mt-0.5 h-5 w-5"/>
+              <span>Purchase</span>
+            </div>
+          </HashLink>
+        </span>
+
+        <Console className="p-4 m-0">
+          <pre data-prefix="$ "><code>npm i daisyui</code></pre> 
+          <pre data-prefix="> " className="text-warning"><code>installing...</code></pre> 
+          <pre data-prefix="> " className="text-success"><code>Done!</code></pre>
+        </Console>
       </span>
-      <div className="pt-34 mockup-code">
-      <pre data-prefix="$"><code>npm i daisyui</code></pre> 
-      <pre data-prefix=">" className="text-warning"><code>installing...</code></pre> 
-      <pre data-prefix=">" className="text-success"><code>Done!</code></pre>
-      </div>
     </div>
   );
 }
