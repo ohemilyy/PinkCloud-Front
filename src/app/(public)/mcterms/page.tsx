d92c1fd38f2a4c01bf01f8e9ae4343d42ca0ca71
Luna <emily@bunni.me>
2024-05-31 18:12:53 -0400

    pui

---
+++ src/app/(public)/mcterms/page.tsx
@@ -0,0 +1,139 @@
+import './styles.css';
+
+export default function Terms() {
+  return (
+    <section id="terms" className="flex flex-col items-center justify-center gap-y-6 w-full h-full p-16 pt-32">
+      <div className="flex flex-col gap-y-2 max-w-[1180px]">
+        <h1 className="text-rainbow text-4xl md:text-5xl lg:text-6xl font-medium font-header lg:max-w-[72rem] mx-auto">
+          Terms of Service
+        </h1>
+        <p className="text-warning font-header font-medium mx-auto">
+          Effective Date:  May 31st, 2024 | Last Updated:  May 31st, 2024
+        </p>
+
+        <div className="flex flex-col md:gap-y-4 mt-10">
+          <p>
+            Welcome to LunarLabs! By accessing or using our services, you agree to comply with and be bound by these terms.
+          </p>
+          <p>
+            These Terms of Service (&quot;Terms&quot;) govern your use of the services provided by LunarLabs (the &quot;Collective&quot;, the &quot;Company&quot;) through our services.
+            By accessing and/or using our software and/or website and the services provided through it (collectively, the &quot;Services&quot;), 
+            you agree to comply with and be bound by the following terms and conditions of use (&quot;Terms of Service&quot;). If you do not agree to these Terms of Service, you should not use our Services.
+          </p>
+          <h4 className="font-bold text-secondary">1. Acceptance of Terms</h4>
+          <p>
+            By using our Services, you acknowledge that you have read, understood, and agree to be bound by these Terms of Service, as well as our Privacy Policy.
+          </p>
+          <h4 className="font-bold text-secondary">2. Chargeback Policy</h4>
+          <p>
+          Attempting to initiate a &quot;Chargeback&quot; will result in immediate termination of your access to LunarLabs services/products. 
+          Refunds are not provided unless explicitly stated otherwise.
+          </p>
+          <h4 className="font-bold text-secondary">3. Redistribution Prohibition</h4>
+          <p>
+          Re-distributing (&quot;leaking&quot;, &quot;sharing&quot;, &quot;re-posting&quot;, &quot;re-publishing&quot;, &quot;spreading&quot;) the service will 
+          result in termination of your access to the Service. 
+          </p>
+          <h4 className="font-bold text-secondary">4. Tampering and De-obfuscation Policy</h4>
+          <p>
+          De-obfuscating (&quot;un-hiding&quot;, &quot;un-protecting&quot;, &quot;dumping&quot;, &quot;cracking&quot;) the service or tampering with Gravity will result in termination of your access to the Service.
+          </p>
+          <h4 className="font-bold text-secondary">5. Limitation of Liability</h4>
+          <p>
+          We are not liable for any damage caused by attempting to re-distribute (&quot;leak&quot;, &quot;share&quot;, &quot;post&quot;) access to the service or accessing its 
+          obfuscated (&quot;hidden&quot;, &quot;protected&quot;, &quot;private&quot;) code. Nor are we liable for repercussions resulting from such actions.  
+          </p>
+          <h4 className="font-bold text-secondary">6. Use of Leaked Versions</h4>
+          <p>
+          We are not liable for any damage caused by the attempted use of a re-distributed (&quot;leaked&quot;, &quot;shared&quot;, &quot;posted&quot;) access to the 
+          service or de-obfuscated code (&quot;loader&quot;, &quot;gravity&quot;, &quot;dumped jar&quot;). Nor are we liable for repercussions resulting from such actions.  
+          </p>
+          <h4 className="font-bold text-secondary">7. Modification of Terms</h4>
+          <p>
+           We reserve the right to modify these Terms of Service without prior announcement or re-agreement. It is your responsibility to stay updated with our terms/rules.
+          </p>
+          <h4 className="font-bold text-secondary">8. No Refunds Policy</h4>
+          <p>
+          No refunds will be issued once the allotted which is a 2 day refund policy for any issues that might happen making one of our products incompatible with your server.  
+          </p>
+          <h4 className="font-bold text-secondary">9. Acceptance</h4>
+          <p>
+          By launching, loading, enabling, running, downloading a copy of any of our digital products (&quot;Gravity&quot;, &quot;Hydro AntiCheat&quot;, 
+          &quot;AntiCheat Software&quot;, "&quot;Universe&quot;, &quot;Nova&quot;,&quot;PlayerServers&quot;) or by posting a message inside our private (&quot;buyer only&quot;, 
+          &quot;customer only&quot;, &quot;enterprise only&quot;) Discord server, you agree to these Terms.  
+          </p>
+          <h5 className="text-rainbow text-4xl md:text-5xl lg:text-6xl font-medium font-header lg:max-w-[72rem] mx-auto">
+          Specific Clauses
+          </h5>
+          <h4 className="font-bold text-secondary">1. Termination</h4>
+          <p>
+          We reserve the right to terminate your access to our service immediately, without prior notice or liability, 
+          for any reason, including breach of these Terms.
+          </p>
+          <h4 className="font-bold text-secondary">2. Indemnity Clause</h4>
+          <p>
+          You agree to defend, indemnify, and hold harmless LunarLabs and its directors, affiliates, and agents from any claims, liabilities, damages, 
+          losses, and expenses arising from your use of the service.
+          </p>
+          <h4 className="font-bold text-secondary">3. Ownership</h4>
+          <p>
+          You are the legal owner of your license to your LunarLabs product. Redistribution of the service requires verification from an official LunarLabs Director.
+          </p>
+          <h4 className="font-bold text-secondary">4. Source Code</h4>
+          <h6><b>Code Ownership and Specificity:</b></h6>
+          <ul className="list-disc list-inside">
+            <li>
+              The Developer(s) retains ownership and copyright of the source code.
+            </li>
+            </ul>
+          <h6><b>Source Code License:</b></h6>
+          <h6><b>Standard Practice:</b></h6>
+          <ul className="list-disc list-inside">
+            <li>
+              The Developer(s) does not sell source code; the product is sold.
+            </li>
+            <li>
+              Clients have the right to use the product but cannot modify the source code.
+            </li>
+            <li>
+              Extra modifications not specified initially will be charged separately.
+            </li>
+          </ul>
+          <h6><b>Negotiable License:</b></h6>
+          <ul className="list-disc list-inside">
+            <li>
+              Clients with purchased source code rights will have full access to the code.
+            </li>
+            <li>
+              Ownership and copyright of the code remain with the Developer, but a license is granted to the Client.
+            </li>
+            <li>
+              Extra fees will be negotiated and applied for each case.
+            </li>
+          </ul>
+          <p>
+            If you have any questions about these Terms, please contact us at <a className="text-secondary" href="mailto:info@lunarlabs.cc">info@lunarlabs.cc</a>.
+          </p>
+          <h4 className="font-bold text-secondary">5. Privacy</h4>
+          <p>
+          We reserve the right to keep our intellectual property hidden. Any malicious interaction with the obfuscated code will result in punitive action determined 
+          by LunarLabs Management.
+          </p>
+          <h4 className="font-bold text-secondary">6. User Protection</h4>
+          <p>
+          Users are prohibited from releasing any information deemed from the our Enterprise discord server. Violation of this policy will result in termination of service access.
+          </p>
+          <h4 className="font-bold text-secondary">7. Data Collection</h4>
+          <p>
+          We collect various data related to server activity and user interactions with our software. This includes server startup, backend IPs. 
+          Attempts to de-obfuscate or use disabled copies of the service will be recorded.
+          </p>
+          <h4 className="font-bold text-secondary">8. Gravity</h4>
+          <p>
+          Our Discord application is used for licensing purposes and should not be modified, investigated, or redistributed.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
